f8b579e1caf36162da7f0c559044410e8ef0e915
Mohamed Haidar <mohamed.haidar@extendaretail.com>
2023-01-14 23:39:45 +0100

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+import axios from 'axios';
+
+import { About } from './about';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [result, setResult] = useState("Hello, what is your question master");
+  const chatbotApi = axios.create({
+    baseURL: "https://api.openai.com/v1",
+    headers: {
+      Authorization:
+        "Bearer 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",
+    },
+  });
+
+  const updateData = (e) => {
+    setData(e.target.value);
+  };
+
+  const ITCallHome = async () => {
+    const result = await chatbotApi.post("/completions", {
+      model: "text-davinci-003",
+      prompt: data,
+      max_tokens: 500,
+      temperature: 0,
+    });
+
+    console.log(result.data);
+    setResult(result.data.choices[0].text); // This is the response from the API
+  };
+
   return (
     <div className="App">
+      <About />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={ITCallHome}>Call Home</button>
+        <input type="text" onChange={updateData} />
+        <p>{result}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
